a907b8e7b507e9a548464d7eeaed0b4dfd5a7fda
Cuiqun Chen <1013757040@qq.com>
2023-06-21 18:21:46 +0800

    Add files via upload

---
+++ utils/metrics.py
@@ -0,0 +1,290 @@
+import torch
+import numpy as np
+import os
+from utils.reranking import re_ranking
+import logging
+
+def VOCap(rec, prec):
+    mrec = np.append(0, rec)
+    mrec = np.append(mrec, 1)
+
+    mpre = np.append(0, prec)
+    mpre = np.append(mpre, 0)
+
+    for ii in range(len(mpre)-2,-1,-1):
+        mpre[ii] = max(mpre[ii], mpre[ii+1])
+
+    msk = [i!=j for i,j in zip(mrec[1:], mrec[0:-1])]
+    ap = np.sum((mrec[1:][msk]-mrec[0:-1][msk])*mpre[1:][msk])
+    return ap
+
+def eval_AP_inner(inst_id, scores, gt_labels, top=None):
+    pos_flag = gt_labels == inst_id
+    tot = scores.shape[0]
+    tot_pos = np.sum(pos_flag)
+
+    sort_idx = np.argsort(-scores)
+    tp = pos_flag[sort_idx]
+    fp = np.logical_not(tp)
+
+    if top is not None:
+        top = min(top, tot)
+        tp = tp[:top]
+        fp = fp[:top]
+        tot_pos = min(top, tot_pos)
+
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+    try:
+        rec = tp / tot_pos
+        prec = tp / (tp + fp)
+    except:
+        print(inst_id, tot_pos)
+        return np.nan
+
+    ap = VOCap(rec, prec)
+    return ap
+
+def eval_precision(inst_id, scores, gt_labels, top=100):
+    pos_flag = gt_labels == inst_id
+    tot = scores.shape[0]
+
+    top = min(top, tot)
+
+    sort_idx = np.argsort(-scores)
+    return np.sum(pos_flag[sort_idx][:top])/top
+
+
+def evlua2(scores, predicted_features_query, gt_labels_query,gt_labels_gallery,get_precision=True):
+        # logger = logging.getLogger("transreid.eva")
+        # logger.info('start testing')
+        
+        mAP_ls = [[] for _ in range(len(np.unique(gt_labels_query)))]
+        for fi in range(predicted_features_query.shape[0]):
+            mapi = eval_AP_inner(gt_labels_query[fi], scores[fi], gt_labels_gallery)
+            mAP_ls[gt_labels_query[fi]].append(mapi)
+
+        # print('calculating average mAP')
+        # for mAPi,mAPs in enumerate(mAP_ls):
+        #     print(str(mAPi)+' '+str(np.nanmean(mAPs))[:5]+' '+str(np.nanstd(mAPs))[:5])
+        all_AP = sum(mAP_ls, [])
+        # print('Average mAP: {} {}'.format(str(np.nanmean(all_AP))[:5], str(np.nanstd(all_AP))[:5]))
+        # logger.info('Average mAP: {} {}'.format(str(np.nanmean(all_AP))[:5], str(np.nanstd(all_AP))[:5]))
+
+        if get_precision:
+
+            prec_ls = [[] for _ in range(len(np.unique(gt_labels_query)))]
+            prec_ls_1 = [[] for _ in range(len(np.unique(gt_labels_query)))]
+            for fi in range(predicted_features_query.shape[0]):
+                prec = eval_precision(gt_labels_query[fi], scores[fi], gt_labels_gallery)
+                prec_ls[gt_labels_query[fi]].append(prec)
+                
+                prec = eval_precision(gt_labels_query[fi], scores[fi], gt_labels_gallery, top=1)
+                prec_ls_1[gt_labels_query[fi]].append(prec)
+            # print('calculating average precision')
+
+            # for preci,precs in enumerate(prec_ls):
+            #     print(str(preci)+' '+str(np.nanmean(precs))[:5]+' '+str(np.nanstd(precs))[:5])
+            all_prec = sum(prec_ls, [])
+            all_prec_1 = sum(prec_ls_1, [])
+
+            # print('Average precision: {} {}'.format(str(np.nanmean(all_prec))[:5], str(np.nanstd(all_prec))[:5]))
+            # logger.info('Average precision: {} {}'.format(str(np.nanmean(all_prec))[:5], str(np.nanstd(all_prec))[:5]))
+        
+        return np.nanmean(all_AP), np.nanstd(all_AP), np.nanmean(all_prec), np.nanstd(all_prec)
+
+def euclidean_distance(qf, gf):
+    m = qf.shape[0]
+    n = gf.shape[0]
+    dist_mat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+               torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+    dist_mat.addmm_(1, -2, qf, gf.t())
+    return dist_mat.cpu().numpy()
+
+def cosine_similarity(qf, gf):
+    epsilon = 0.00001
+    dist_mat = qf.mm(gf.t())
+    qf_norm = torch.norm(qf, p=2, dim=1, keepdim=True)  # mx1
+    gf_norm = torch.norm(gf, p=2, dim=1, keepdim=True)  # nx1
+    qg_normdot = qf_norm.mm(gf_norm.t())
+
+    dist_mat = dist_mat.mul(1 / qg_normdot).cpu().numpy()
+    dist_mat = np.clip(dist_mat, -1 + epsilon, 1 - epsilon)
+    dist_mat = np.arccos(dist_mat)
+    return dist_mat
+
+
+def eval_func(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=200):
+    """Evaluation with market1501 metric
+        Key: for each query identity, its gallery images from the same camera view are discarded.
+        """
+    num_q, num_g = distmat.shape
+    # distmat g
+    #    q    1 3 2 4
+    #         4 1 2 3
+    if num_g < max_rank:
+        max_rank = num_g
+        print("Note: number of gallery samples is quite small, got {}".format(num_g))
+    indices = np.argsort(distmat, axis=1)
+    #  0 2 1 3
+    #  1 2 3 0
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0.  # number of valid query
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query
+        # order = indices[q_idx]  # select one row
+        # remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        # keep = np.invert(remove)
+
+        # compute cmc curve
+        # binary vector, positions with value 1 are correct matches
+        orig_cmc = matches[q_idx]#[keep]
+        if not np.any(orig_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = orig_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = orig_cmc.sum()
+        tmp_cmc = orig_cmc.cumsum()
+        #tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
+        y = np.arange(1, tmp_cmc.shape[0] + 1) * 1.0
+        000
+        
+        tmp_cmc = tmp_cmc / y
+        tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
+
+
+def eval_pku(distmat, q_pids, g_pids, max_rank=20):
+    num_q, num_g = distmat.shape
+    if num_g < max_rank:
+        max_rank = num_g
+        print("Note: number of gallery samples is quite small, got {}".format(num_g))
+    indices = np.argsort(distmat, axis=1)
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    num_valid_q = 0.  # number of valid query
+
+    # only two cameras
+    q_camids = np.ones(num_q).astype(np.int32)
+    g_camids = 2 * np.ones(num_g).astype(np.int32)
+
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query
+        order = indices[q_idx]
+        remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        keep = np.invert(remove)
+
+        # compute cmc curve
+        raw_cmc = matches[q_idx][keep]  # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = raw_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+
+    return all_cmc, mAP
+
+class R1_mAP_eval():
+    def __init__(self, cfg, num_query, max_rank=50, feat_norm=True, reranking=False):
+        super(R1_mAP_eval, self).__init__()
+        self.num_query = num_query
+        self.max_rank = max_rank
+        self.feat_norm = feat_norm
+        self.reranking = reranking
+        self.cfg = cfg
+
+    def reset(self):
+        self.feats = []
+        self.pids = []
+        self.camids = []
+
+    def update(self, output):  # called once for each batch
+        feat, pid, camid = output
+        self.feats.append(feat.cpu())
+        self.pids.extend(np.asarray(pid))
+        self.camids.extend(np.asarray(camid.detach().cpu()))
+
+    def compute(self):  # called after each epoch
+        feats = torch.cat(self.feats, dim=0)
+        if self.feat_norm:
+            print("The test feature is normalized")
+            feats = torch.nn.functional.normalize(feats, dim=1, p=2)  # along channel
+        # query
+        qf = feats[:self.num_query]
+        q_pids = np.asarray(self.pids[:self.num_query])
+        q_camids = np.asarray(self.camids[:self.num_query])
+        # gallery
+        gf = feats[self.num_query:]
+        g_pids = np.asarray(self.pids[self.num_query:])
+
+        g_camids = np.asarray(self.camids[self.num_query:])
+        if self.reranking:
+            print('=> Enter reranking')
+            # distmat = re_ranking(qf, gf, k1=20, k2=6, lambda_value=0.3)
+            distmat = re_ranking(qf, gf, k1=50, k2=15, lambda_value=0.3)
+
+        else:
+            print('=> Computing DistMat with euclidean_distance')
+            distmat = euclidean_distance(qf, gf)
+        
+        # import pdb
+        # pdb.set_trace()
+        if self.cfg.DATASETS.NAMES in ['tuberlin', 'sketchy']:
+            m_m, m_s, p_m, p_s = evlua2(-distmat, qf, q_pids, g_pids)
+            return m_m, m_s, p_m, p_s
+        else:
+            cmc, mAP = eval_func(distmat, q_pids, g_pids, q_camids, g_camids)
+            return cmc, mAP
+
+
+
